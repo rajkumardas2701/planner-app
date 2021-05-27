068f5564afe32c9a248bd5f9eb62c6579ee4372a
Rajkumar Das <rajkumardas2701@gmail.com>
2021-05-28 03:51:07 +0530

    Add task form structre

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,9 +1,59 @@
-const AddTask = () => (
-  <div>
-    <h3>
-      Add Task
-    </h3>
-  </div>
-);
+import { useState } from 'react';
+
+const AddTask = () => {
+  const [taskform, setTaskForm] = useState({
+    id: 0,
+    name: '',
+    toDo: 'To do',
+    dueDate: '',
+    assign: '',
+    progress: '',
+  });
+  const taskSubmit = () => {
+
+  };
+
+  const handleChange = ({ target: { name, value } }) => {
+    setTaskForm({ ...taskform, [name]: value });
+  };
+  return (
+    <div>
+      <form onSubmit={taskSubmit}>
+        <input
+          placeholder="Enter a task name"
+          name="name"
+          type="text"
+          value={taskform.name}
+          onChange={handleChange}
+          required
+        />
+        <input
+          placeholder="To do"
+          name="toDo"
+          type="text"
+          value={taskform.toDo}
+          onChange={handleChange}
+        />
+        <input
+          placeholder="Set due date"
+          name="dueDate"
+          type="date"
+          value={taskform.dueDate}
+          onChange={handleChange}
+        />
+        <input
+          placeholder="Assign"
+          name="assign"
+          type="text"
+          value={taskform.assign}
+          onChange={handleChange}
+        />
+        <button type="button">
+          Add Task
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default AddTask;

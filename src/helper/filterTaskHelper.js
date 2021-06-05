bc7184c5644e166e9709dd1895571719630a843b
Rajkumar Das <rajkumardas2701@gmail.com>
2021-06-05 07:53:27 +0530

    Add filter logic to show common task on multiple selection

--- src/helper/filterTaskHelper.js
+++ src/helper/filterTaskHelper.js
@@ -67,6 +67,7 @@ const filterTaskHelper = (tasks, filters) => {
   const perDue = fetchPerDueDate(tasks);
   const perProgress = fetchPerProgress(tasks);
 
+  // when none of the due date filter is selected
   if (dueFilters.every((ele) => ele === false)) {
     const dt = [];
     for (let i = 0; i < PROGRESSHELPERARR.length; i += 1) {
@@ -97,12 +98,64 @@ const filterTaskHelper = (tasks, filters) => {
     return TaskList;
   }
 
-  // if (progressFilters.every((ele) => ele === false)) {
+  // when none of the progress filter is selected
+  if (progressFilters.every((ele) => ele === false)) {
+    const pt = [];
+    for (let i = 0; i < DUEHELPERARR.length; i += 1) {
+      if (filters[DUETASKARR[i]] === true && perDue[DUEHELPERARR[i]].length > 0) {
+        dueTasks[DUETASKARR[i]] = perDue[DUEHELPERARR[i]];
+        console.log('Inside Per due loop', perDue[DUEHELPERARR[i]]);
+        if (dueTasks[DUETASKARR[i]]) {
+          for (let j = 0; j < perDue[DUEHELPERARR[i]].length; j += 1) {
+            console.log('due status in every element', perDue[DUEHELPERARR[i]][j].progress);
+            console.log('every element of per due', perDue[DUEHELPERARR[i]][j]);
+            pt.push(perDue[DUEHELPERARR[i]][j]);
+          }
+        }
+      }
+    }
+    // console.log(pt);
+    console.log('final pts', pt);
+    console.log('final due Tasks', dueTasks);
+    if (pt.length > 0) {
+      const progressInDues = fetchPerProgress(pt);
+      for (let i = 0; i < PROGRESSHELPERARR.length; i += 1) {
+        progressTasks[PROGRESSTASKARR[i]] = progressInDues[PROGRESSHELPERARR[i]];
+      }
+    }
+    TaskList.push(dueTasks);
+    TaskList.push(progressTasks);
+    console.log('inside task helper with filters', TaskList);
+    return TaskList;
+  }
 
-  // }
-  for (let i = 0; i < DUEHELPERARR.length; i += 1) {
-    if (filters[DUETASKARR[i]] === true && perDue[DUEHELPERARR[i]].length > 0) {
-      dueTasks[DUETASKARR[i]] = perDue[DUEHELPERARR[i]];
+  // common in progress and DueDate view
+  const commonInProgressAndDues = [];
+  for (let i = 0; i < PROGRESSHELPERARR.length; i += 1) {
+    if (filters[PROGRESSTASKARR[i]] === true
+      && perProgress[PROGRESSHELPERARR[i]].length > 0) {
+      for (let j = 0; j < DUEHELPERARR.length; j += 1) {
+        if (filters[DUETASKARR[j]] === true && perDue[DUEHELPERARR[j]].length > 0) {
+          // progressPresentInDues.push(per);
+          // duesPresentInprogress.push();
+          console.log('inside common condition', perDue[DUEHELPERARR[j]][0].progress);
+          for (let k = 0; k < perDue[DUEHELPERARR[j]].length; k += 1) {
+            if (PROGRESSTASKARR[i] === perDue[DUEHELPERARR[j]][k].progress) {
+              commonInProgressAndDues.push(perDue[DUEHELPERARR[j]][k]);
+            }
+          }
+        }
+      }
+    }
+  }
+  if (commonInProgressAndDues.length > 0) {
+    const commonProgress = fetchPerProgress(commonInProgressAndDues);
+    const commonDues = fetchPerDueDate(commonInProgressAndDues);
+    for (let i = 0; i < PROGRESSHELPERARR.length; i += 1) {
+      progressTasks[PROGRESSTASKARR[i]] = commonProgress[PROGRESSHELPERARR[i]];
+    }
+    for (let i = 0; i < DUEHELPERARR.length; i += 1) {
+      dueTasks[DUETASKARR[i]] = commonDues[DUEHELPERARR[i]];
     }
   }
   TaskList.push(dueTasks);

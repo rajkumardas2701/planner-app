80e81b8d8b7d4bcce36fe5f5fb1b9fdbabec9bc7
Rajkumar Das <rajkumardas2701@gmail.com>
2021-06-10 03:27:15 +0530

    Add style for task and addTask form

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,35 +1,62 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import moment from 'moment';
+import '../styles/Task.css';
 
-const Task = ({ todo }) => (
-  <ul>
-    <li>
-      id
-      {todo.id}
-    </li>
-    <li>
-      name
-      {todo.name}
-    </li>
-    <li>
-      To do
-      {todo.toDo}
-    </li>
-    <li>
-      Due date
-      {todo.dueDate}
-    </li>
-    <li>
-      Assign
-      {todo.assign}
-    </li>
-    <li>
-      progress
-      {todo.progress}
-    </li>
-    {/* {console.log('from task component', todo)} */}
-  </ul>
-);
+const Task = ({ todo }) => {
+  const fetchClass = (progress) => {
+    if (progress === 'Not started') {
+      return 'progress-view-not-started';
+    } else if (progress === 'In progress') {
+      return 'progress-view-in-progress';
+    }
+    return 'progress-view-completed';
+  }
+  const handleProgressState = (todo) => {
+    
+  };
+  return (
+    <div className="task-container">
+      <div className="task-title-status">
+        <button type="button"
+         className={fetchClass(todo.progress)}
+         onClick={handleProgressState}
+         />
+        <p className="task-title">
+          {todo.name}
+        </p>
+      </div>
+      <div>
+        {
+          (todo.toDo && todo.toDo.length > 0)
+          ? (<div className="todoTag">{todo.toDo}</div>)
+          : (<p className="todoTag">To do</p>)
+        }
+      </div>
+      {
+        (todo.dueDate && todo.dueDate.length > 0)
+        ? (      
+          <div className="task-dueDate">
+            <i className="far fa-calendar-alt cal-task"/>
+            <p>{moment(todo.dueDate, 'YYYY-MM-DD').format('MM/DD')}</p>
+          </div> 
+        )
+        : ''
+        }
+        {
+          (todo.assign && todo.assign.length > 0)
+          ? (      
+              <div className="task-assigned">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7fy7sKAqTLiD5YtPEY84Q9wtV8dDLmoBSeg&usqp=CAU" alt="avator" />
+                <p>{todo.assign}</p>
+              </div> 
+          )
+          : ''
+        }
+    </div>
+  );
+};
+
 
 Task.propTypes = {
   todo: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,

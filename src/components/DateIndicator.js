dbe24da01c10d6ba664e6124d9e456f8f6bb7c81
Rajkumar Das <rajkumardas2701@gmail.com>
2021-06-13 07:39:26 +0530

    Add feature of showing link to extra tasks in calendar

--- src/components/DateIndicator.js
+++ src/components/DateIndicator.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {
   getDayOfMonth,
   getMonth,
   getYear,
 } from '../helper/moment-helper';
+import moment from 'moment';
 import { getDatesAndEvents } from '../helper/calendarHelper';
 import '../styles/month-row.css';
 
 const DateIndicator = ({ selectDate, events }) => {
+  const [showExtraTask, setShowExtraTask] = useState(false);
   const datesAndEvents = getDatesAndEvents(
     getMonth(selectDate) + 1,
     getYear(selectDate),
     events,
   );
+  const filterEventsOnDay = (events) => {
+    const firstTwo = [];
+    console.log('events', events);
+    for (let i = 0; i < 2; i += 1) {
+      firstTwo.push(events[i]);
+    }
+    return firstTwo;
+  };
+  const handleExtraTask = (e) => {
+    e.preventDefault();
+    setShowExtraTask(!showExtraTask);
+  }
   // document.querySelector('div[data-active-month=false]').style
   // console.log(setSelectDate);
   // console.log('dayEvents in DateIndicator', datesAndEvents);
@@ -30,7 +44,45 @@ const DateIndicator = ({ selectDate, events }) => {
         </p>
         <div className="date-task">
           {
-            i.dayEvents.map((event) => <p key={event.id} className="each-task">{event.name}</p>)
+            (i.dayEvents.length <= 2)
+            ? (<div>
+              {
+                i.dayEvents.map((event) => <p key={event.id} className="each-task">{event.name}</p>)
+              }
+            </div>)
+            : (
+              <div>
+                {
+                  <div className="with-extra-task-container">
+                    <div>
+                      {
+                        filterEventsOnDay(i.dayEvents).map((event) => <p key={event.id} className="each-task">{event.name}</p>)
+                      }
+                    </div>
+                    <div className="extra-btn-section">
+                      {<button type='button' className="link-to-extra" onClick={handleExtraTask}>+{i.dayEvents.length - 2}{' '}more</button>}
+                    </div>
+                    {
+                      showExtraTask &&
+                      (
+                        <div className="list-extra-task">
+                          <div className="list-tasks-date-section">
+                            <div>{moment(i.dayEvents[0].dueDate).format('MMMM D, YYYY')}</div>
+                            <button type="button" onClick={handleExtraTask}>X</button>
+                          </div>
+                          
+                          <div>
+                            {
+                              i.dayEvents.map((event) => <p key={event.id} className="each-task">{event.name}</p>)
+                            }
+                          </div>
+                        </div>
+                      )
+                    }
+                  </div>
+                }
+              </div>
+            )
           }
         </div>
       </div>
